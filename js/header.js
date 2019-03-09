ef36b3968d1f0245805a68715c8afd2d290e6047
VASIA <Vasia.danilyuk@gmail.com>
2019-03-09 19:08:28 +0200

    adding modal

--- js/header.js
+++ js/header.js
@@ -1,5 +1,6 @@
-var menu_button = document.getElementsByClassName('hitem')[0];
-
+let menu_button = document.getElementsByClassName('hitem')[0];
+let elem = document.getElementById("opensing");
+let elem1 = document.getElementsByClassName("modal")[0];
 function open_left_menu() {
 
     if (document.getElementsByClassName('leftbar')[0].style.left === "-280px") {
@@ -16,5 +17,17 @@ function open_left_menu() {
     }
 
 }
+function openmodal() {
+    document.getElementsByClassName("modal-content")[0].style = ("display:block");
+    document.getElementsByClassName("modal")[0].style = ("display:block");
 
+}
+
+function closemodal() {
+    document.getElementsByClassName("modal-content")[0].style = ("display:none");
+    document.getElementsByClassName("modal")[0].style = ("display:none;");
+
+}
+elem.onclick = openmodal;
+elem1.onclick = closemodal;
 menu_button.onclick = open_left_menu;
